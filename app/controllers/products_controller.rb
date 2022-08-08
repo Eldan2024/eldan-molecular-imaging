6116d18212d818231ec1b1ef55893cc7e8e3084a
Tomer Gelbhart <gelbharttomer@gmail.com>
2022-08-08 20:43:18 +0200

    added dropdown subject list

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,55 @@
 class ProductsController < ApplicationController
   def ga67
+    @breadcrumb = "Ga-67"
+    @title = "Gallium citrate (Ga-67) injection"
+    @desc = "Ut ipsum cillum sunt enim. Nisi mollit eu aliqua mollit laboris ea anim velit commodo aute occaecat ea. Ipsum quis exercitation fugiat consectetur adipisicing ut. Irure non quis dolore anim cupidatat commodo cupidatat cillum ad. Nulla labore aliquip magna pariatur ea elit exercitation incididunt ipsum."
+
+    @question1 = "Example Question 1"
+    @answer1 = "Nunc duis id aenean gravida tincidunt eu, tempor ullamcorper. Viverra aliquam arcu, viverra et, cursus. Aliquet pretium cursus adipiscing gravida et consequat lobortis arcu velit. Nibh pharetra fermentum duis accumsan lectus non. Massa cursus molestie lorem scelerisque pellentesque. Nisi, enim, arcu purus gravida adipiscing euismod montes, duis egestas. Vehicula eu etiam quam tristique tincidunt suspendisse ut consequat."
+
+    @question2 = "Example Question 2"
+    @answer2 = "Nunc duis id aenean gravida tincidunt eu, tempor ullamcorper. Viverra aliquam arcu, viverra et, cursus. Aliquet pretium cursus adipiscing gravida et consequat lobortis arcu velit. Nibh pharetra fermentum duis accumsan lectus non. Massa cursus molestie lorem scelerisque pellentesque. Nisi, enim, arcu purus gravida adipiscing euismod montes, duis egestas. Vehicula eu etiam quam tristique tincidunt suspendisse ut consequat."
+
+    @question3 = "Example Question 3"
+    @answer3 = "Nunc duis id aenean gravida tincidunt eu, tempor ullamcorper. Viverra aliquam arcu, viverra et, cursus. Aliquet pretium cursus adipiscing gravida et consequat lobortis arcu velit. Nibh pharetra fermentum duis accumsan lectus non. Massa cursus molestie lorem scelerisque pellentesque. Nisi, enim, arcu purus gravida adipiscing euismod montes, duis egestas. Vehicula eu etiam quam tristique tincidunt suspendisse ut consequat."
+
+    @question4 = "Example Question 4"
+    @answer4 = "Nunc duis id aenean gravida tincidunt eu, tempor ullamcorper. Viverra aliquam arcu, viverra et, cursus. Aliquet pretium cursus adipiscing gravida et consequat lobortis arcu velit. Nibh pharetra fermentum duis accumsan lectus non. Massa cursus molestie lorem scelerisque pellentesque. Nisi, enim, arcu purus gravida adipiscing euismod montes, duis egestas. Vehicula eu etiam quam tristique tincidunt suspendisse ut consequat."
   end
 
   def mo99
+    @breadcrumb = "⁹⁹Mo/⁹⁹ᵐTc"
+    @title = "⁹⁹Mo/⁹⁹ᵐTc Generator"
+    @desc = "The Technetium 99m Generator is used to obtain a sterile, non-pyrogenic supply of Tc-99m as Sodium Pertechnetate. The generator consists of a previously sterilized column containing Molybdenum-99, absorbed on Alumina. The generator maybe eluted whenever sufficient quantities of Tc 99m have accumulated in the column."
+
+    @question1 = "Example Question 1"
+    @answer1 = "Nunc duis id aenean gravida tincidunt eu, tempor ullamcorper. Viverra aliquam arcu, viverra et, cursus. Aliquet pretium cursus adipiscing gravida et consequat lobortis arcu velit. Nibh pharetra fermentum duis accumsan lectus non. Massa cursus molestie lorem scelerisque pellentesque. Nisi, enim, arcu purus gravida adipiscing euismod montes, duis egestas. Vehicula eu etiam quam tristique tincidunt suspendisse ut consequat."
+
+    @question2 = "Example Question 2"
+    @answer2 = "Nunc duis id aenean gravida tincidunt eu, tempor ullamcorper. Viverra aliquam arcu, viverra et, cursus. Aliquet pretium cursus adipiscing gravida et consequat lobortis arcu velit. Nibh pharetra fermentum duis accumsan lectus non. Massa cursus molestie lorem scelerisque pellentesque. Nisi, enim, arcu purus gravida adipiscing euismod montes, duis egestas. Vehicula eu etiam quam tristique tincidunt suspendisse ut consequat."
+
+    @question3 = "Example Question 3"
+    @answer3 = "Nunc duis id aenean gravida tincidunt eu, tempor ullamcorper. Viverra aliquam arcu, viverra et, cursus. Aliquet pretium cursus adipiscing gravida et consequat lobortis arcu velit. Nibh pharetra fermentum duis accumsan lectus non. Massa cursus molestie lorem scelerisque pellentesque. Nisi, enim, arcu purus gravida adipiscing euismod montes, duis egestas. Vehicula eu etiam quam tristique tincidunt suspendisse ut consequat."
+
+    @question4 = "Example Question 4"
+    @answer4 = "Nunc duis id aenean gravida tincidunt eu, tempor ullamcorper. Viverra aliquam arcu, viverra et, cursus. Aliquet pretium cursus adipiscing gravida et consequat lobortis arcu velit. Nibh pharetra fermentum duis accumsan lectus non. Massa cursus molestie lorem scelerisque pellentesque. Nisi, enim, arcu purus gravida adipiscing euismod montes, duis egestas. Vehicula eu etiam quam tristique tincidunt suspendisse ut consequat."
   end
 
   def tl201
+    @breadcrumb = "²⁰¹TI"
+    @title = "Thallium Chloride (²⁰¹TI) injection"
+    @desc = "Ut ipsum cillum sunt enim. Nisi mollit eu aliqua mollit laboris ea anim velit commodo aute occaecat ea. Ipsum quis exercitation fugiat consectetur adipisicing ut. Irure non quis dolore anim cupidatat commodo cupidatat cillum ad. Nulla labore aliquip magna pariatur ea elit exercitation incididunt ipsum."
+
+    @question1 = "Example Question 1"
+    @answer1 = "Nunc duis id aenean gravida tincidunt eu, tempor ullamcorper. Viverra aliquam arcu, viverra et, cursus. Aliquet pretium cursus adipiscing gravida et consequat lobortis arcu velit. Nibh pharetra fermentum duis accumsan lectus non. Massa cursus molestie lorem scelerisque pellentesque. Nisi, enim, arcu purus gravida adipiscing euismod montes, duis egestas. Vehicula eu etiam quam tristique tincidunt suspendisse ut consequat."
+
+    @question2 = "Example Question 2"
+    @answer2 = "Nunc duis id aenean gravida tincidunt eu, tempor ullamcorper. Viverra aliquam arcu, viverra et, cursus. Aliquet pretium cursus adipiscing gravida et consequat lobortis arcu velit. Nibh pharetra fermentum duis accumsan lectus non. Massa cursus molestie lorem scelerisque pellentesque. Nisi, enim, arcu purus gravida adipiscing euismod montes, duis egestas. Vehicula eu etiam quam tristique tincidunt suspendisse ut consequat."
+
+    @question3 = "Example Question 3"
+    @answer3 = "Nunc duis id aenean gravida tincidunt eu, tempor ullamcorper. Viverra aliquam arcu, viverra et, cursus. Aliquet pretium cursus adipiscing gravida et consequat lobortis arcu velit. Nibh pharetra fermentum duis accumsan lectus non. Massa cursus molestie lorem scelerisque pellentesque. Nisi, enim, arcu purus gravida adipiscing euismod montes, duis egestas. Vehicula eu etiam quam tristique tincidunt suspendisse ut consequat."
+
+    @question4 = "Example Question 4"
+    @answer4 = "Nunc duis id aenean gravida tincidunt eu, tempor ullamcorper. Viverra aliquam arcu, viverra et, cursus. Aliquet pretium cursus adipiscing gravida et consequat lobortis arcu velit. Nibh pharetra fermentum duis accumsan lectus non. Massa cursus molestie lorem scelerisque pellentesque. Nisi, enim, arcu purus gravida adipiscing euismod montes, duis egestas. Vehicula eu etiam quam tristique tincidunt suspendisse ut consequat."
   end
 end
