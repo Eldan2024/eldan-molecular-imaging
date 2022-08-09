1a8139f81dca47f562e55540e8de0233f50b6560
Tomer Gelbhart <gelbharttomer@gmail.com>
2022-08-09 16:52:58 +0200

    added mailing server

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -28,32 +28,36 @@ class PagesController < ApplicationController
 
     @member1_name = "Ariel Ikan"
     @member1_pos = ""
-    @member1_desc = ""
     @member1_linkedin = "https://www.linkedin.com/in/ariel-ikan/"
     @member1_image = @member1_name.split(' ').second.downcase + ".png"
+    @member1_desc = ""
 
     @member2_name = "Tal Rabinovich"
     @member2_pos = "General Manager"
-    @member2_desc = "Tal Rabinovitch joined the Neopharm Group in 2007 and was appointed as Eldan’s GM in 2013. Previously he served as VP for Business Development and Operations in LDD Advanced Technologies for 5 years. In 2012 he joined Eldan and was appointed VP for Strategic Development. Prior to joining the group Mr. Rabinovich gained experience in business development and strategic planning while working in Shaldor, a strategy and management consulting firm. Mr. Rabinovitch holds an MBA degree as well as M.Sc. and B.Sc. degrees in Electrical Engineering."
     @member2_linkedin = "https://www.linkedin.com/in/tal-rabinovich-659a54/"
     @member2_image = @member2_name.split(' ').second.downcase + ".png"
+    @member2_desc = "Tal Rabinovitch joined the Neopharm Group in 2007 and was appointed as Eldan’s GM in 2013. Previously he served as VP for Business Development and Operations in LDD Advanced Technologies for 5 years. In 2012 he joined Eldan and was appointed VP for Strategic Development.
+
+Prior to joining the group Mr. Rabinovich gained experience in business development and strategic planning while working in Shaldor, a strategy and management consulting firm. Mr. Rabinovitch holds an MBA degree as well as M.Sc. and B.Sc. degrees in Electrical Engineering."
 
     @member3_name = "Bareket Gelbhart"
     @member3_pos = "International Business Manager"
-    @member3_desc = "Bareket Gelbhart joined Eldan Molecular Imaging in January 2022 as the International Business Manager. Previously she worked in Dipharma, serving as Head of Marketing for 6 years and as Chief Business Development Officer for additional 3 years. Prior to Dipharma, Bareket worked in Teva Pharmaceuticals for 17 years, in different positions."
     @member3_linkedin = "https://www.linkedin.com/in/bareket-gelbhart-83904a16/"
     @member3_image = @member3_name.split(' ').second.downcase + ".png"
+    @member3_desc = "Bareket Gelbhart joined Eldan Molecular Imaging in January 2022 as the International Business Manager. Previously she worked in Dipharma, serving as Head of Marketing for 6 years and as Chief Business Development Officer for additional 3 years.
+
+Prior to Dipharma, Bareket worked in Teva Pharmaceuticals for 17 years, in different positions."
 
     @member4_name = "Ronit Duanis"
     @member4_pos = ""
-    @member4_desc = ""
     @member4_linkedin = ""
     @member4_image = @member4_name.split(' ').second.downcase + ".png"
+    @member4_desc = ""
 
     @member5_name = "Haim Shoshan"
     @member5_pos = ""
-    @member5_desc = ""
     @member5_linkedin = ""
     @member5_image = @member5_name.split(' ').second.downcase + ".png"
+    @member5_desc = ""
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -21,4 +21,6 @@ Rails.application.routes.draw do
 
   match "/404", to: "errors#not_found", via: :all
   match "/500", to: "errors#internal_server_error", via: :all
+
+  resources :contacts, only: [:new, :create]
 end
