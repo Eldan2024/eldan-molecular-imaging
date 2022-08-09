da4c9266f8f74ad46676912b6d7c36e0570f99f4
Tomer Gelbhart <gelbharttomer@gmail.com>
2022-08-09 22:03:00 +0200

    updated page routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,25 @@
 Rails.application.routes.draw do
   root 'pages#index'
 
-  get 'products/ga67'
+  get 'products', to: redirect('products/mo99')
   get 'products/mo99'
+  get 'products/ga67'
   get 'products/tl201'
 
-  get 'news/index'
+  get 'news', to: 'news#index'
   get 'news/example'
 
   get 'partners', controller: 'pages'
   get 'team', controller: 'pages'
 
-  get 'about/eldan'
+  get 'about', to: redirect('about/molecular_imaging')
   get 'about/molecular_imaging'
+  get 'about/eldan'
   get 'about/neopharm'
 
-  get 'shared/_header'
-  get 'shared/_footer'
+  match '/404', to: 'errors#not_found', via: :all
+  match '/500', to: 'errors#internal_server_error', via: :all
 
-  match "/404", to: "errors#not_found", via: :all
-  match "/500", to: "errors#internal_server_error", via: :all
-
-  get 'new', controller: 'contacts', as: 'contacts'
   resources :contacts, only: [:new, :create]
+  get 'contacts', to: 'contacts#new'
 end
