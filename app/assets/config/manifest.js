810292ef58b0dde39a8c524a1fafcf054a45f496
Tomer Gelbhart <gelbharttomer@gmail.com>
2022-08-10 10:49:24 +0200

    Update manifest.js

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,7 +1,26 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
-//= link_tree ../favicon
-//= link_tree ../json
-//= link_tree ../vendor
+
+//= link_directory ../vendor/boxicons/css .css
+//= link_directory ../vendor/boxicons/css
+//= link_directory ../vendor/boxicons
+//= link_directory ../vendor
+//= link_directory ../vendor/bootstrap/dist/js
+//= link_directory ../vendor/smooth-scroll/dist
+//= link_directory ../vendor/cleave.js/dist
+//= link_directory ../vendor/swiper
+//= link_directory ../vendor/lightgallery/css
+//= link_directory ../vendor/jarallax/dist
+//= link_directory ../vendor/lightgallery
+//= link_directory ../vendor/lightgallery/plugins
+//= link_directory ../vendor/lightgallery/plugins/fullscreen
+//= link_directory ../vendor/lightgallery/plugins/zoom
+//= link_directory ../vendor/lightgallery/plugins/video
+//= link_directory ../vendor/lightgallery/plugins/thumbnail
+//= link_directory ../vendor/@lottiefiles/lottie-player/dist
+//= link_directory ../favicon
+//= link_directory ../json
+
+//= link_directory ../vendor
 //= link_tree ../images
