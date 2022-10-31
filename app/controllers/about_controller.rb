8c9d0ba0424cac452057cbaf14a4e817ea94d8c0
Tom <67170933+Gelbh@users.noreply.github.com>
2022-10-31 14:28:54 +0000

    fixed accessibility for index, header and footer

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,22 +1,22 @@
 class AboutController < ApplicationController
   def eldan
-    @title = "Eldan / About"
+    @title = "Eldan Electronic Instruments Ltd. / About"
     @desc = "Eldan Molecular Imaging"
     @jarallax = "about_cover"
-    @logo = "logo_eldan.png"
+    @logo = "logo_eldan"
   end
 
   def molecular_imaging
     @title = "Eldan Molecular Imaging / About"
     @desc = "Eldan Molecular Imaging"
     @jarallax = "about_cover"
-    @logo = "logo.png"
+    @logo = "logo"
   end
 
   def neopharm_group
     @title = "Neopharm Group / About"
     @desc = "Eldan Molecular Imaging"
     @jarallax = "about_cover"
-    @logo = "logo_neopharm.png"
+    @logo = "logo_neopharm"
   end
 end
