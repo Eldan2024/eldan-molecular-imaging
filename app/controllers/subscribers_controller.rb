73f820438a59acaebf31c825d91472efccc1ef49
Tom <67170933+Gelbh@users.noreply.github.com>
2022-11-24 03:03:08 +0000

    Update subscribers_controller.rb

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -8,23 +8,7 @@ class SubscribersController < ApplicationController
     @subscriber = Subscriber.new(subscriber_params)
 
     dc = 'us12'
-    unique_id = "4c89d991c8"
-    url = "https://#{dc}.api.mailchimp.com/3.0/lists/#{unique_id}/members"
     api_key = "aec5127eb440a4db5442b1cd94619f30-us12"
-
-    user_details = {
-      email: params[:email],
-      status: "subscribed",
-    };
-
-    conn = Faraday.new(
-      url: url,
-      headers: {'Content-Type' => 'application/json', 'Authorization': "Bearer #{api_key}"}
-    )
-
-    conn.post() do |req|
-      req.body = user_details.to_json
-    end
 
     if @subscriber.save
       cookies[:saved_lead] = true
