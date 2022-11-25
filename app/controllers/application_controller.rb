b5b7b7751ea648ef47ce6b99a061421bf6b94951
Tom <67170933+Gelbh@users.noreply.github.com>
2022-11-25 00:33:35 +0000

    minor changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,2 @@
 class ApplicationController < ActionController::Base
-  @desc = "Eldan Molecular Imaging"
-
-  if defined?(@title) 
-    @title = @title + " / Eldan MI"
-  else 
-    @title = "Eldan MI"
-  end 
 end
