9a4e94a17d008caf8e6b01c91902151a8ccbe2ce
Tom <67170933+Gelbh@users.noreply.github.com>
2022-11-27 04:31:34 +0000

    modified sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,18 +1,18 @@
 SitemapGenerator::Sitemap.default_host = "http://eldanmi.com"
 SitemapGenerator::Sitemap.create do
-  add distribution_path, :changefreq => 'monthly'
-  add team_path, :changefreq => 'monthly'
-  add news_path, :changefreq => 'monthly'
-  add contacts_path, :changefreq => 'monthly'
+  add '/distribution', :changefreq => 'monthly'
+  add '/team', :changefreq => 'monthly'
+  add '/news', :changefreq => 'monthly'
+  add '/contacts', :changefreq => 'monthly'
 
-  add accessability_declaration_path, :changefreq => 'yearly'
-  add privacy_policy_path, :changefreq => 'yearly'
+  add '/privacy_policy', :changefreq => 'yearly'
+  add '/accessability_declaration', :changefreq => 'yearly'
 
-  add products_tc99_path, :changefreq => 'monthly'
-  add products_ga67_path, :changefreq => 'monthly'
-  add products_tl201_path, :changefreq => 'monthly'
+  add '/products/tc99', :changefreq => 'monthly'
+  add '/products/tl201', :changefreq => 'monthly'
+  add '/products/ga67', :changefreq => 'monthly'
 
-  add about_molecular_imaging_path, :changefreq => 'monthly'
-  add about_eldan_path, :changefreq => 'monthly'
-  add about_neopharm_group_path, :changefreq => 'monthly'
+  add '/about/molecular_imaging', :changefreq => 'monthly'
+  add '/about/eldan', :changefreq => 'monthly'
+  add '/about/neopharm_group', :changefreq => 'monthly'
 end
