9ade9be472f3440c37876cd957cba162ada54867
gelbh <gelbharttomer@gmail.com>
2024-01-01 18:53:36 +0100

    migrated to enterprise recaptcha

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,5 @@
 Recaptcha.configure do |config|
-  config.site_key  = '6LcnYSAjAAAAAMXWJhypF1iUXfunyivdRyv-Rvp6'
-  config.secret_key = '6LcnYSAjAAAAAEJoJHHTGj5XAI0blHZDuFAsam1N'
+  config.enterprise = 'true'
+  config.enterprise_api_key = ENV['RECAPTCHA_ENTERPRISE_API_KEY']
+  config.enterprise_project_id = 'EldanMI'
 end
