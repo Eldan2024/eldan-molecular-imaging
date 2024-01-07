b830c161e985469d1eaa216b70cc54054a3e494f
gelbh <gelbharttomer@gmail.com>
2024-01-07 17:02:17 +0100

    fix(views): Refactored some views

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,19 +1,19 @@
 class ErrorsController < ApplicationController
   def not_found
-    @description = "Error 404 - The page you are looking for was moved, removed or might have never existed."
     @title = "Error 404"
-    @heading = "Error 404"
-
+    @description = "Error 404 - The page you are looking for was moved, removed or might have never existed."
+    
+    @heading = @title
     @message = "The page you are looking for was moved, removed or might have never existed."
 
     render status: 404
   end
 
   def internal_server_error
-    @description = "Error 500 - Oops, something went wrong. Try to refresh this page or feel free to contact us if the problem persists."
     @title = "Error 500"
-    @heading = "Error 500"
+    @description = "Error 500 - Oops, something went wrong. Try to refresh this page or feel free to contact us if the problem persists."
 
+    @heading = @title
     @message = "Oops, something went wrong.<br>Try to refresh this page or feel free to contact us if the problem persists".html_safe
     
     render status: 500
